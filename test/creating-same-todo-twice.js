620a7bc561b5c0d10dcc011f12611f1ef87a52d4
Matthew Andrews <matt@mattandre.ws>
2014-08-16 23:51:52 +0100

    Switch to using a time stamp as uuid - fixes #2

---
+++ test/creating-same-todo-twice.js
@@ -0,0 +1,36 @@
+// Test bits
+var request = require('supertest');
+
+// Component under test
+var todos = require('../lib/todos');
+
+// Scaffolding and mocking
+var express = require('express');
+var app = express();
+var Datastore = require('nedb');
+var db = new Datastore();
+app.use('/todos', todos(db));
+
+describe('creating multiple todos with the same id', function() {
+  var now = Date.now();
+  it('after creating a normal todo', function(done) {
+    request(app)
+      .post('/todos')
+      .send({ text: 'Wash the dishes', created: now, updated: now })
+      .expect(201, done);
+  });
+
+  it('should not allow the creation of another todo with the same id', function(done) {
+    request(app)
+      .post('/todos')
+      .send({ text: 'Wash the dishes', created: now, updated: now })
+      .expect(409, done);
+  });
+
+  it('after deleting the todo', function(done) {
+    request(app)
+      .delete('/todos/'+now)
+      .send({ text: 'Wash the dishes', created: now, updated: now })
+      .expect(202, done);
+  });
+});
