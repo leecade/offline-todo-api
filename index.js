3a4eb6eb0253aae92fa2335a8bbdf3ac133fa5a6
Matthew Andrews <matt@mattandre.ws>
2014-08-16 09:19:00 +0100

    Heroku compat

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ var express = require('express');
 var Datastore = require('nedb');
 var todos = require('./lib/todos');
 var cors= require('cors');
+var port = Number(process.env.PORT || 3000);
 
 var db = new Datastore({ filename: 'todos', autoload: true });
 express()
   .use(cors())
   .use('/todos', todos(db))
-  .listen(3000);
+  .listen(port);
+console.log('listening on '+port);
